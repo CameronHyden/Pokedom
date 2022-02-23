6389c910199f6e9a52114c5240245b98a07836e8
CameronHyden <camh.hyden@gmail.comcamh.hyden@gmail.comclear>
2022-02-23 19:37:41 +0000

    html-shows-pokedex

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-import { pokemonArray } from "./data/pokemon.js";
+import pokemonArray from "./data/pokemon.js";
 
-const pokeContainer = document.querySelector(".card-container")
-for(let poke of pokemonArray) {
-    pokeContainer.innerText = poke 
-}
+const cardContainer = document.querySelector(".card-container");
 
-const pokeList = `<div class = "card-container"
-    <h1>${pokemonArray.id}</h1>
-    <h2>${pokemonArray.name}</h2>
-    <p>${pokemonArray.type}</p>
-    <img src=${pokemonArray.sprite} </img>
-    </div>`;
+const makeFirstLetterCapital = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
 
+const getTypesText = (types) => {
+  if (types.length === 1) {
+    return types[0];
+  }
 
-pokeContainer.innerText = pokeList ; 
+  if (types.length === 2) {
+    return `${types[0]} & ${types[1]}`;
+  }
 
+  console.error("unhandled type array");
+};
 
+const createPokemonCards = () => {
+  cardContainer.innerHTML = "";
+
+  pokemonArray.forEach((pokemon) => {
+    const nameWithCapital = makeFirstLetterCapital(pokemon.name);
+    const typesText = getTypesText(pokemon.types);
+
+    cardContainer.innerHTML += `
+	<div class="card">
+		<img
+			class="card__image"
+			src="${pokemon.sprite}"
+		/>
+		<div class="card__content">
+			<h2 class="card__heading">${nameWithCapital}</h2>
+			<p class="card__text">
+				${nameWithCapital} (#${pokemon.id}) is a ${typesText} type pokemon.
+	nameWithCApital
+	</div>`;
+  });
+};
+
+createPokemonCards();
